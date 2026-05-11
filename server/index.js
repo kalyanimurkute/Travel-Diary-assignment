db12aa839ae809fc47268fd2589522eb4ba84d58
kalyanimurkute <murkutekalyani259@gmail.com>
2026-05-11 11:12:01 +0530

    add uri

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://travel-diary-assignment-1.onrender.com",
 }));
 
 app.use(express.json());
